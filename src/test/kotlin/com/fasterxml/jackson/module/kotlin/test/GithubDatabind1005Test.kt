8d9fb7a9db8d624c776ae6f4cdf4f8ce3b254c3e
apatrida <jayson.minard@gmail.com>
2015-11-17 15:38:44 -0300

    added test case clarifying issue in Slack added test case for Github jackson-databind issue #1005

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/GithubDatabind1005Test.kt
@@ -0,0 +1,24 @@
+package com.fasterxml.jackson.module.kotlin.test
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.annotation.*
+import org.junit.Ignore
+import org.junit.Test
+
+public class TestHiddenKotlinThings {
+
+    // make a class seem kinda like a Java class, but with a second generated constructor
+    // this is Ugly Kotlin but is making it easier for Java people to see and debug.
+    class Something @JsonCreator constructor (@JsonProperty("name") name: String,
+                                              @JsonProperty("age") age: Int = 0) { // default value causes synthetic constructor to be generated
+        public val name: String = name
+        public val age: Int = age
+    }
+
+    @Ignore("This isn't a problem with the Kotlin module unless someone refuses to use it, then constructor selection gets confused over synthetic constructors")
+    @Test public fun testSyntheticGeneratedConstructorIsIgnored() {
+       val thing: Something = ObjectMapper().readValue("""{"name":"fred","age":99}""")
+    }
+}
