75501dfba47d605ef9ad326db158a24ae269a9c9
apatrida <jayson.minard@gmail.com>
2016-06-06 09:31:30 -0300

    added TODO with "tough case" for #27, not sure it will ever be handled.

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github27.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github27.kt
@@ -15,6 +15,7 @@ class TestGithub27 {
     val mapper = jacksonObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, false)
 
     private data class ClassWithNullableInt(val sample: Int?)
+
     @Test fun testNullableInt() {
         val json = """{"sample":null}"""
         val stateObj = mapper.readValue<ClassWithNullableInt>(json)
@@ -22,6 +23,7 @@ class TestGithub27 {
     }
 
     private data class ClassWithInt(val sample: Int)
+
     @Test fun testInt() {
         val json = """{"sample":null}"""
         val stateObj = mapper.readValue<ClassWithInt>(json)
@@ -29,6 +31,7 @@ class TestGithub27 {
     }
 
     private data class ClassWithListOfNullableInt(val samples: List<Int?>)
+
     @Test fun testListOfNullableInt() {
         val json = """{"samples":[1, null]}"""
         val stateObj = mapper.readValue<ClassWithListOfInt>(json)
@@ -52,5 +55,9 @@ class TestGithub27 {
         @get:JsonIgnore val safeSamples: List<Int> by lazy { samples.filterNotNull() }
     }
 
-
+    // TODO:  this would get tougher to nullable check, tough problem to solve
+    class ClassWithNonNullableT<T>(val something: T,
+                                   val listSomething: List<T>,
+                                   val mapOfListOfSomething: Map<String, List<T>>,
+                                   val innerSomething: ClassWithNonNullableT<T>?)
 }
