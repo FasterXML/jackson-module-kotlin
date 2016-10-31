02f8d486076a77aec3ebed9cd09e469b1adcc5a7
apatrida <jayson.minard@gmail.com>
2016-10-30 21:08:18 -0300

    add test case for #47, currently marked ignored until databind 1438 is resolved

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github47.kt
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.module.kotlin.test
+
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.module.kotlin.*
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@Ignore("Waiting for fix to https://github.com/FasterXML/jackson-databind/issues/1438")
+class Github47 {
+
+    class ConfigItem(val configItemId: String)
+
+    @Test
+    fun testCaseInsensitivePropertyNames() {
+        val mapper = jacksonObjectMapper()
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+
+        val jsonWithMismtachedPropertyName = """
+                    {
+                        "configItemID": "test"
+                    }
+                   """
+
+        val item: ConfigItem = mapper.readValue<ConfigItem>(jsonWithMismtachedPropertyName)
+        assertEquals("test", item.configItemId)
+    }
+}
