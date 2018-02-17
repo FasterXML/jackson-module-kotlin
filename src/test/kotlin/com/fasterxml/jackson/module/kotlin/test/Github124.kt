e4c389411b0b227928987db5fa0ee919051a6308
apatrida <jayson.minard@gmail.com>
2018-02-16 21:29:55 -0600

    Unit tests that we cannot do anything about, but keeping to note the issues

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github124.kt
@@ -0,0 +1,43 @@
+package com.fasterxml.jackson.module.kotlin.test
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Ignore
+import org.junit.Test
+
+internal class Github124 {
+    @Test
+    @Ignore("This is as expected, cannot use the two annotations in concert")
+    fun test() {
+        val objMapper = jacksonObjectMapper()
+
+        // with 2.9.3 prints
+        //     Foo(name=foo, query=NonSerializable, rawQuery=bar)
+        // but with 2.9.4 throws:
+        //     com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException: Instantiation of [simple type, class DeserializationTest$Foo] value failed for JSON property query due to missing (therefore NULL) value for creator parameter rawQuery which is a non-nullable type
+        //      at [Source: (String)"{"name": "foo", "query": "bar"}"; line: 1, column: 31] (through reference chain: DeserializationTest$Foo["query"])
+        val deserialized: Foo = objMapper.readValue("{\"name\": \"foo\", \"query\": \"bar\"}")
+        println(deserialized)
+
+        val serialized = objMapper.writeValueAsString(deserialized)
+
+        // with 2.9.3 prints
+        //     {"name":"foo","query":"bar"}
+        println(serialized)
+    }
+
+    class NonSerializable(private val field: Any?) {
+        override fun toString() = "NonSerializable"
+    }
+
+    data class Foo(@JsonProperty("name") val name: String,
+                   @JsonIgnore val query: NonSerializable,
+                   @JsonProperty("query") val rawQuery: String) {
+        @JsonCreator
+        constructor(@JsonProperty("name") name: String,
+                    @JsonProperty("query") rawQuery: String): this(name, NonSerializable(rawQuery), rawQuery)
+    }
+}
