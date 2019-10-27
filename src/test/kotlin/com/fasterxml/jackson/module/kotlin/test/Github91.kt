fb2e2f566664dfa4f419f0c4480c448129761118
apatrida <39ee8d8de9f9607bb1e84ae6d27cde373ff22ab3>
2019-10-27 16:28:59 -0600

    Show Github #91 test and how it should work.

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github91.kt
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.module.kotlin.test
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Github91Test {
+
+    data class DataClass1(val name: String, val content: DataClass2)
+
+    data class DataClass2 @JsonCreator(mode = JsonCreator.Mode.DELEGATING) constructor (val content: String)
+
+    private val jsonData = """
+        {
+            "name": "my name",
+            "content": "some value"
+        }
+        """
+
+    @Test
+    fun testJsonParsing() {
+        val mapper = jacksonObjectMapper()
+        val dataClass1 = mapper.readValue<DataClass1>(jsonData)
+        assertEquals(DataClass1("my name", DataClass2("some value")), dataClass1)
+    }
+}
