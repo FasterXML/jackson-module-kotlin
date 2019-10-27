bf3cd3284408472bb2951dfb80f1bba6e28d9ec0
apatrida <39ee8d8de9f9607bb1e84ae6d27cde373ff22ab3>
2019-10-27 16:36:41 -0600

    fix Github #91 for serialization, and test that case

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github91.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github91.kt
@@ -1,6 +1,7 @@
 package com.fasterxml.jackson.module.kotlin.test
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.Assert.assertEquals
@@ -10,7 +11,7 @@ class Github91Test {
 
     data class DataClass1(val name: String, val content: DataClass2)
 
-    data class DataClass2 @JsonCreator(mode = JsonCreator.Mode.DELEGATING) constructor (val content: String)
+    data class DataClass2 @JsonCreator(mode = JsonCreator.Mode.DELEGATING) constructor (@JsonValue val content: String)
 
     private val jsonData = """
         {
@@ -24,5 +25,6 @@ class Github91Test {
         val mapper = jacksonObjectMapper()
         val dataClass1 = mapper.readValue<DataClass1>(jsonData)
         assertEquals(DataClass1("my name", DataClass2("some value")), dataClass1)
+        assertEquals("{\"name\":\"my name\",\"content\":\"some value\"}", mapper.writeValueAsString(dataClass1))
     }
 }
