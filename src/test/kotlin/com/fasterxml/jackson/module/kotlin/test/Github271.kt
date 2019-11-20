f6ecdf3496ba64507362188d35f1a7dfd6e1fb5b
Edd Steel <edward@treasure-data.com>
2019-11-20 11:09:46 -0800

    Add failing test for issue 271

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/Github271.kt
@@ -0,0 +1,21 @@
+package com.fasterxml.jackson.module.kotlin.test
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class TestGithub271 {
+    @JsonPropertyOrder(alphabetic=true)
+    data class Foo(val a: String, val c: String) {
+        val b = "b"
+    }
+
+    @Test
+    fun testAlphabeticFields() {
+        val mapper = jacksonObjectMapper()
+
+        val json = mapper.writeValueAsString(Foo("a","c"))
+        assertEquals("""{"a":"a","b":"b","c":"c"}""", json)
+    }
+}
