21de487dd6180122dca29dd4ca00beba53467699
Drew Stephens <drew@dinomite.net>
2020-03-08 15:30:51 -0600

    Update test for singleton support

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/ObjectSingletonTest.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/ObjectSingletonTest.kt
@@ -1,18 +1,16 @@
 package com.fasterxml.jackson.module.kotlin.test
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 // [module-kotlin#225]: keep Kotlin singletons as singletons
 class TestObjectSingleton {
-
-    val mapper: ObjectMapper = jacksonObjectMapper()
+    val mapper: ObjectMapper = ObjectMapper()
+        .registerModule(KotlinModule(enableExperimentalSingletonSupport = true))
 
     object Singleton {
         var content = 1 // mutable state
@@ -49,9 +47,8 @@ class TestObjectSingleton {
         val newSingleton = mapper.readValue<Singleton>(js)
         assertThat(newSingleton.content, equalTo(Singleton.content))
 
-        newSingleton.content += 1;
+        newSingleton.content += 1
 
         assertThat(Singleton.content, equalTo(newSingleton.content))
     }
-
 }
