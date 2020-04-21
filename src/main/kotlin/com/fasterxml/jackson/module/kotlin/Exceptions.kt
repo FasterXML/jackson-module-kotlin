8b69890883e5b650d15ba9331738c845d8ffc631
Drew Stephens <drew@dinomite.net>
2020-04-21 13:14:07 -0400

    Make MKPE descend from MismatchedInputException

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/Exceptions.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/Exceptions.kt
@@ -1,10 +1,14 @@
 package com.fasterxml.jackson.module.kotlin
 
+import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.JsonMappingException
-import java.io.Closeable
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import kotlin.reflect.KParameter
 
 /**
- * Specialized [JsonMappingException] sub-class used to indicate that a mandatory Kotlin constructor parameter was missing or null.
+ * Specialized [JsonMappingException] sub-class used to indicate that a mandatory Kotlin constructor
+ * parameter was missing or null.
  */
-class MissingKotlinParameterException(val parameter: KParameter, val processor: Closeable? = null, val msg: String) : JsonMappingException(processor, msg)
+class MissingKotlinParameterException(val parameter: KParameter,
+                                      val processor: JsonParser? = null,
+                                      val msg: String) : MismatchedInputException(processor, msg)
