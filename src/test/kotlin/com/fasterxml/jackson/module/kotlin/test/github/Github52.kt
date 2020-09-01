fef93234764a1151df7074ed5e26fbd4bc6ada41
Drew Stephens <drew@dinomite.net>
2020-09-01 09:48:46 -0400

    Split into separate tests so it's easy to see what's broken

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github52.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github52.kt
@@ -1,48 +1,64 @@
 package com.fasterxml.jackson.module.kotlin.test.github
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.MapperFeature
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.test.assertEquals
 
 class TestGithub52 {
-    class Test(
-            // Should only be serialized as "is_bar"
-            @JsonProperty("is_bar")
-            val bar: Boolean = true,
+    private val mapper = jacksonObjectMapper()
 
-            @JsonProperty("is_bar2")
-            val isBar2: Boolean = true,
+    @org.junit.Test
+    fun testBooleanPropertyInConstructor() {
+        data class BooleanPropertyInConstructor(
+                @JsonProperty("is_bar")
+                val bar: Boolean = true
+        )
 
-            // This gets serialized as "is_lol", as expected. No issues here.
-            @JsonProperty("is_lol")
-            val lol: String = "sdf"
-    ) {
-        // Should only be serialized as "is_foo"
-        @JsonProperty("is_foo")
-        val foo: Boolean = true
-
-        @JsonProperty("is_foo2")
-        val isFoo2: Boolean = true
+        assertEquals("""{"is_bar":true}""", mapper.writeValueAsString(BooleanPropertyInConstructor()))
     }
 
     @org.junit.Test
-    fun testGithub52() {
-        val mapper = jacksonObjectMapper()
-                .configure(SerializationFeature.INDENT_OUTPUT, true)
-                .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true)
+    fun testIsPrefixedBooleanPropertyInConstructor() {
+        data class IsPrefixedBooleanPropertyInConstructor(
+                @JsonProperty("is_bar2")
+                val isBar2: Boolean = true
+        )
 
-        val actual = mapper.writeValueAsString(Test())
-        val expected = """
-            {
-              "is_bar":true,
-              "is_bar2":true,
-              "is_lol":"sdf",
-              "is_foo":true,
-              "is_foo2":true
-            }""".trimIndent()
+        assertEquals("""{"is_bar2":true}""", mapper.writeValueAsString(IsPrefixedBooleanPropertyInConstructor()))
+    }
 
-        assertEquals(expected, actual)
+    @org.junit.Test
+    fun testIsPrefixedStringPropertyInConstructor() {
+        data class IsPrefixedStringPropertyInConstructor(
+                @JsonProperty("is_lol")
+                val lol: String = "sdf"
+        )
+
+        assertEquals("""{"is_lol":"sdf"}""", mapper.writeValueAsString(IsPrefixedStringPropertyInConstructor()))
+    }
+
+    @org.junit.Test
+    fun testBooleanPropertyInBody() {
+        data class BooleanPropertyInBody(
+                @JsonIgnore val placeholder: String = "placeholder"
+        ) {
+            @JsonProperty("is_foo")
+            val foo: Boolean = true
+        }
+
+        assertEquals("""{"is_foo":true}""", mapper.writeValueAsString(BooleanPropertyInBody()))
+    }
+
+    @org.junit.Test
+    fun testIsPrefixedBooleanPropertyInBody() {
+        data class IsPrefixedBooleanPropertyInBody(
+                @JsonIgnore val placeholder: String = "placeholder"
+        ) {
+            @JsonProperty("is_foo2")
+            val isFoo2: Boolean = true
+        }
+
+        assertEquals("""{"is_foo2":true}""", mapper.writeValueAsString(IsPrefixedBooleanPropertyInBody()))
     }
 }
