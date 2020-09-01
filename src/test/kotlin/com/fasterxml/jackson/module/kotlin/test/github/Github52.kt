02cfe51c874aa4319b4333776b101f3dcd066f5c
Drew Stephens <drew@dinomite.net>
2020-09-01 09:24:23 -0400

    Clarify test

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github52.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github52.kt
@@ -1,23 +1,25 @@
 package com.fasterxml.jackson.module.kotlin.test.github
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.test.assertEquals
 
 class TestGithub52 {
     class Test(
-        // This gets serialized as "is_lol", as expected. No issues here.
-        @JsonProperty("is_lol")
-        val lol: String = "sdf",
+            // Should only be serialized as "is_bar"
+            @JsonProperty("is_bar")
+            val bar: Boolean = true,
 
-        // This gets serialized as "bar", even though we asked for "is_bar". This is an issue.
-        @JsonProperty("is_bar")
-        val bar: Boolean = true,
+            @JsonProperty("is_bar2")
+            val isBar2: Boolean = true,
 
-        @JsonProperty("is_bar2")
-        val isBar2: Boolean = true) {
-
-        // This gets serialized as both "foo" and "is_foo", although we only asked for "is_foo". Also an issue.
+            // This gets serialized as "is_lol", as expected. No issues here.
+            @JsonProperty("is_lol")
+            val lol: String = "sdf"
+    ) {
+        // Should only be serialized as "is_foo"
         @JsonProperty("is_foo")
         val foo: Boolean = true
 
@@ -28,17 +30,19 @@ class TestGithub52 {
     @org.junit.Test
     fun testGithub52() {
         val mapper = jacksonObjectMapper()
+                .configure(SerializationFeature.INDENT_OUTPUT, true)
+                .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true)
 
         val actual = mapper.writeValueAsString(Test())
-        val expected = """{"is_lol":"sdf","is_bar":true,"is_bar2":true,"is_foo":true,"is_foo2":true}"""
+        val expected = """
+            {
+              "is_bar":true,
+              "is_bar2":true,
+              "is_lol":"sdf",
+              "is_foo":true,
+              "is_foo2":true
+            }""".trimIndent()
 
-        // error
-        // {"is_lol":"sdf",
-        //  "is_bar":true,
-        //  "bar2":true,  ... should be is_bar2
-        //  "foo2":true,  ... should not be here
-        //  "is_foo":true,
-        //  "is_foo2":true}
         assertEquals(expected, actual)
     }
 }
