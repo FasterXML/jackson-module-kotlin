ac91202184a682fb42bc17076f7c2a2850d61c42
Drew Stephens <drew@dinomite.net>
2020-12-22 06:03:31 -0500

    Fix package

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github356.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github356.kt
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.module.kotlin.test.failing
+package com.fasterxml.jackson.module.kotlin.test.github
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
