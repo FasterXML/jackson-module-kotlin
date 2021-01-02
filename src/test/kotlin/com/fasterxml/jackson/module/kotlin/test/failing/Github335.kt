28407869ba92bcbc9710d1b3e1a96069a13c47fd
Drew Stephens <drew@dinomite.net>
2021-01-02 12:53:11 -0500

    Add failing test for Github issue #335

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/failing/Github335.kt
@@ -0,0 +1,35 @@
+package com.fasterxml.jackson.module.kotlin.test.failing
+
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Test
+import kotlin.test.assertNull
+
+class Github335Test {
+    val mapper = jacksonObjectMapper()
+
+    interface Payload
+    data class UniquePayload(val data: String) : Payload
+
+    data class MyEntity(
+            val type: String?,
+            @JsonTypeInfo(use = Id.NAME, include = As.EXTERNAL_PROPERTY, property = "type")
+            @JsonSubTypes(Type(value = UniquePayload::class, name = "UniquePayload"))
+            val payload: Payload?
+    )
+
+    @Test
+    fun serializeAndDeserializeTypeable() {
+        val oldEntity = MyEntity(null, null)
+        val json = mapper.writeValueAsString(oldEntity)
+        val newEntity = mapper.readValue<MyEntity>(json)
+
+        // Returns false: "null" String instead of null
+        assertNull(newEntity.type)
+    }
+}
