787c887f48ce22ae4006aa8d86dc7dbf638e952c
Eric Fenderbosch <eric@hubindustrial.com>
2021-02-26 16:32:21 -0500

    added (de)serializers for Kotlin unsigned number types

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinDeserializers.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinDeserializers.kt
@@ -1,8 +1,10 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE")
+
 package com.fasterxml.jackson.module.kotlin
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken.VALUE_NUMBER_INT
+import com.fasterxml.jackson.core.exc.InputCoercionException
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
@@ -13,7 +15,7 @@ object SequenceDeserializer : StdDeserializer<Sequence<*>>(Sequence::class.java)
     }
 }
 
-object RegexDeserializer: StdDeserializer<Regex>(Regex::class.java) {
+object RegexDeserializer : StdDeserializer<Regex>(Regex::class.java) {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Regex {
         val node = ctxt.readTree(p)
 
@@ -37,14 +39,61 @@ object RegexDeserializer: StdDeserializer<Regex>(Regex::class.java) {
     }
 }
 
-internal class KotlinDeserializers: Deserializers.Base() {
-    override fun findBeanDeserializer(type: JavaType, config: DeserializationConfig?, beanDesc: BeanDescription?): JsonDeserializer<*>? {
-        return if (type.isInterface && type.rawClass == Sequence::class.java) {
-            SequenceDeserializer
-        } else if (type.rawClass == Regex::class.java) {
-            RegexDeserializer
-        } else {
-            null
+object UByteDeserializer : StdDeserializer<UByte>(UByte::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+        p.shortValue.asUByte() ?: throw InputCoercionException(
+            p,
+            "Numeric value (${p.text}) out of range of UByte (0 - ${UByte.MAX_VALUE}).",
+            VALUE_NUMBER_INT,
+            UByte::class.java
+        )
+}
+
+object UShortDeserializer : StdDeserializer<UShort>(UShort::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+        p.intValue.asUShort() ?: throw InputCoercionException(
+            p,
+            "Numeric value (${p.text}) out of range of UShort (0 - ${UShort.MAX_VALUE}).",
+            VALUE_NUMBER_INT,
+            UShort::class.java
+        )
+}
+
+object UIntDeserializer : StdDeserializer<UInt>(UInt::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+        p.longValue.asUInt() ?: throw InputCoercionException(
+            p,
+            "Numeric value (${p.text}) out of range of UInt (0 - ${UInt.MAX_VALUE}).",
+            VALUE_NUMBER_INT,
+            UInt::class.java
+        )
+}
+
+object ULongDeserializer : StdDeserializer<ULong>(ULong::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+        p.bigIntegerValue.asULong() ?: throw InputCoercionException(
+            p,
+            "Numeric value (${p.text}) out of range of ULong (0 - ${ULong.MAX_VALUE}).",
+            VALUE_NUMBER_INT,
+            ULong::class.java
+        )
+}
+
+@ExperimentalUnsignedTypes
+internal class KotlinDeserializers : Deserializers.Base() {
+    override fun findBeanDeserializer(
+        type: JavaType,
+        config: DeserializationConfig?,
+        beanDesc: BeanDescription?
+    ): JsonDeserializer<*>? {
+        return when {
+            type.isInterface && type.rawClass == Sequence::class.java -> SequenceDeserializer
+            type.rawClass == Regex::class.java -> RegexDeserializer
+            type.rawClass == UByte::class.java -> UByteDeserializer
+            type.rawClass == UShort::class.java -> UShortDeserializer
+            type.rawClass == UInt::class.java -> UIntDeserializer
+            type.rawClass == ULong::class.java -> ULongDeserializer
+            else -> null
         }
     }
 }
