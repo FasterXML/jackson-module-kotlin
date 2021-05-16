fce98536a33703ac969aca436b9729bd7ab4e340
wrongwrong <boranti1995@gmail.com>
2021-05-16 14:32:16 +0900

    remove unused import

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/ObjectSingletonTest.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/ObjectSingletonTest.kt
@@ -2,7 +2,6 @@ package com.fasterxml.jackson.module.kotlin.test
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.SingletonSupport
 import com.fasterxml.jackson.module.kotlin.SingletonSupport.CANONICALIZE
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.hamcrest.CoreMatchers.equalTo
