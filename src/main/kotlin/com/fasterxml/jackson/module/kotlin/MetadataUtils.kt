72ae9ae92717833cc9ddfc5532e1393470666f8a
wrongwrong <boranti1995@gmail.com>
2021-05-30 18:04:34 +0900

    util追加

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/MetadataUtils.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/MetadataUtils.kt
@@ -3,8 +3,15 @@ package com.fasterxml.jackson.module.kotlin
 import com.fasterxml.jackson.databind.introspect.Annotated
 import kotlinx.metadata.ClassName
 import kotlinx.metadata.KmClass
+import kotlinx.metadata.KmConstructor
+import kotlinx.metadata.KmFunction
+import kotlinx.metadata.jvm.JvmMethodSignature
 import kotlinx.metadata.jvm.KotlinClassHeader
 import kotlinx.metadata.jvm.KotlinClassMetadata
+import kotlinx.metadata.jvm.signature
+import java.lang.reflect.Constructor
+import java.lang.reflect.Executable
+import java.lang.reflect.Method
 
 internal fun Metadata.toKotlinClassHeader() = KotlinClassHeader(
     this.kind,
@@ -24,4 +31,38 @@ internal fun Annotated.toKmClassOrNull(): KmClass? = this.getAnnotation(Metadata
 // It may fail when executed for types like `kotlin.Int`.
 internal fun ClassName.toJavaClass(): Class<*> {
     return Class.forName(replace(".", "$").replace("/", "."))
+}
+
+private val PRIMITIVE_CLASS_TO_DESC = mapOf(
+    Byte::class.javaPrimitiveType to 'B',
+    Char::class.javaPrimitiveType to 'C',
+    Double::class.javaPrimitiveType to 'D',
+    Float::class.javaPrimitiveType to 'F',
+    Int::class.javaPrimitiveType to 'I',
+    Long::class.javaPrimitiveType to 'J',
+    Short::class.javaPrimitiveType to 'S',
+    Boolean::class.javaPrimitiveType to 'Z',
+    Void::class.javaPrimitiveType to 'V'
+)
+
+// http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3
+private fun Class<*>.toDescriptor(): String = when {
+    isPrimitive -> PRIMITIVE_CLASS_TO_DESC.getValue(this).toString()
+    isArray -> "[${componentType.toDescriptor()}"
+    else -> "L$name;".replace('.', '/')
+}
+
+// Contents of the argument JVM Descriptor with the return type removed.
+private fun Executable.getParametersJvmDescHead() = parameterTypes
+    .joinToString(prefix = "(", separator = "", postfix = ")") { it.toDescriptor() }
+
+// Method to get KmConstructor/KmFunction from Constructor/Method.
+// Forced unwrapping because couldn't find an example where signature can't be obtained from a statically defined class.
+internal fun KmClass.getKmConstructor(constructor: Constructor<*>): KmConstructor =
+    constructors.first { it.signature!!.desc.startsWith(constructor.getParametersJvmDescHead()) }
+// If `Method` is a `getter`/`setter` of a Kotlin property, getting it from `functions` will fail,
+// so it is defined as `nullable`.
+internal fun KmClass.getKmFunction(method: Method): KmFunction? = functions.find {
+    val signature: JvmMethodSignature = it.signature!!
+    signature.name == this.name && signature.desc.startsWith(method.getParametersJvmDescHead())
 }
