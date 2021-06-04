bd17c79f3fa5f6ebb1f4c6d04796f9bf3a821bf3
Drew Stephens <drew@dinomite.net>
2021-06-04 08:43:05 -0400

    Make bitSet internal

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinFeature.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinFeature.kt
@@ -1,15 +1,14 @@
 package com.fasterxml.jackson.module.kotlin
 
-import java.util.*
+import java.util.BitSet
 import kotlin.math.pow
 
 enum class KotlinFeature(val enabledByDefault: Boolean) {
-
     NullToEmptyCollection(enabledByDefault = false),
     NullToEmptyMap(enabledByDefault = false),
     NullIsSameAsDefault(enabledByDefault = false),
     SingletonSupport(enabledByDefault = false),
     StrictNullChecks(enabledByDefault = false);
 
-    val bitSet: BitSet = 2.0.pow(ordinal).toInt().toBitSet()
+    internal val bitSet: BitSet = 2.0.pow(ordinal).toInt().toBitSet()
 }
