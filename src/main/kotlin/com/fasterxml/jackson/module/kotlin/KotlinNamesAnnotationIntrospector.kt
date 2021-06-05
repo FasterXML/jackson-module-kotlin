460923f327e4cb7b2f9f5b409f468d80dd81fd26
wrongwrong <boranti1995@gmail.com>
2021-06-05 18:38:34 +0900

    add getter like method support

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinNamesAnnotationIntrospector.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinNamesAnnotationIntrospector.kt
@@ -37,9 +37,17 @@ internal class KotlinNamesAnnotationIntrospector(val module: KotlinModule, val c
         .takeIf {
             it.parameterCount == 0 && looksLikeKotlinGeneratedMethod(it.name) && !it.hasAnnotation(JvmName::class.java)
         }?.let { _ ->
-            member.declaringClass.kotlin.declaredMemberProperties
-                .find { kProperty -> kProperty.javaGetter == member.member }
-                ?.name
+            member.member.kotlinFunction
+                ?.let { getterLike ->
+                    // for getter like method
+                    val name = getterLike.name
+                    val replaceTarget = if (name.startsWith("get")) "get" else "is"
+
+                    name.substringAfter(replaceTarget).replaceFirstChar { it.lowercase(Locale.getDefault()) }
+                }
+                ?: member.declaringClass.kotlin.declaredMemberProperties // for property getter
+                    .find { kProperty -> kProperty.javaGetter == member.member }
+                    ?.name
         }
 
     // Since getter for value class (inline class) will be compiled into a different name such as "getFoo-${random}".
