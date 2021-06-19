f8e8d82e8f4cafbe86657a7e1506fdd6a50b3836
wrongwrong <boranti1995@gmail.com>
2021-06-20 00:46:45 +0900

    add unbox key serializer

---
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinKeySerializers.kt
@@ -0,0 +1,33 @@
+package com.fasterxml.jackson.module.kotlin
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.ser.Serializers
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.fasterxml.jackson.databind.type.TypeFactory
+
+internal object ValueClassUnboxKeySerializer : StdSerializer<Any>(Any::class.java) {
+    override fun serialize(value: Any, gen: JsonGenerator, provider: SerializerProvider) {
+        val method = value::class.java.getMethod("unbox-impl")
+        val unboxed = method.invoke(value)
+
+        if (unboxed == null) {
+            val javaType = provider.typeFactory.constructType(method.genericReturnType)
+            provider.findNullKeySerializer(javaType, null).serialize(null, gen, provider)
+            return
+        }
+
+        provider.findKeySerializer(unboxed::class.java, null).serialize(unboxed, gen, provider)
+    }
+}
+
+class KotlinKeySerializers : Serializers.Base() {
+    override fun findSerializer(
+        config: SerializationConfig,
+        type: JavaType,
+        beanDesc: BeanDescription
+    ): JsonSerializer<*>? = when {
+        type.rawClass.isUnboxableValueClass() -> ValueClassUnboxKeySerializer
+        else -> null
+    }
+}
