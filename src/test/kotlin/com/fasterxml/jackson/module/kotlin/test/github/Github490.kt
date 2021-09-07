d88a3d6bd356b8c5192ecd967674ed4724cd4dc7
ddomanine <ddomanine@egencia.com>
2021-09-07 23:17:58 +0200

    rebased to 2.13 and applied patch

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github490.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github490.kt
@@ -7,39 +7,61 @@ import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
-class Github490 {
+class TestGithub490 {
+    val mapper = jacksonObjectMapper()
+    val value: DataClassWithAllNullableParams = mapper.readValue(
+        "{" +
+                "\"jsonNodeValueWithNullAsDefaultProvidedNull\":null, " +
+                "\"jsonNodeValueProvidedNull\":null}"
+    )
 
     @Test
-    fun testKotlinDeserialization() {
-        val mapper = jacksonObjectMapper()
-        val value: DataClassWithAllNullableParams = mapper.readValue("{" +
-                "\"jsonNodeValueWithNullAsDefaultProvidedNull\":null, " +
-                "\"jsonNodeValueProvidedNull\":null}")
+    fun testKotlinDeserialization_intValue() {
         assertThat(
             "Nullable missing Int value should be deserialized as null",
             value.intValue,
             CoreMatchers.nullValue()
         )
+    }
+
+    @Test
+    fun testKotlinDeserialization_stringValue() {
         assertThat(
             "Nullable missing String value should be deserialized as null",
             value.stringValue,
             CoreMatchers.nullValue()
         )
+    }
+
+    @Test
+    fun testKotlinDeserialization_jsonNodeValue() {
         assertThat(
             "Nullable missing JsonNode value should be deserialized as null and not as NullNode",
             value.jsonNodeValue,
             CoreMatchers.nullValue()
         )
+    }
+
+    @Test
+    fun testKotlinDeserialization_jsonNodeValueProvidedNull() {
         assertThat(
-            "Nullable missing JsonNode value should be deserialized as null and not as NullNode",
+            "Nullable JsonNode value provided as null should be deserialized as null and not as NullNode",
             value.jsonNodeValueProvidedNull,
             CoreMatchers.nullValue()
         )
+    }
+
+    @Test
+    fun testKotlinDeserialization_jsonNodeValueWithNullAsDefault() {
         assertThat(
             "Nullable by default missing JsonNode value should be deserialized as null and not as NullNode",
             value.jsonNodeValueWithNullAsDefault,
             CoreMatchers.nullValue()
         )
+    }
+
+    @Test
+    fun testKotlinDeserialization_jsonNodeValueWithNullAsDefaultProvidedNull() {
         assertThat(
             "Nullable by default JsonNode with provided null value in payload should be deserialized as null and not as NullNode",
             value.jsonNodeValueWithNullAsDefaultProvidedNull,
