8ee36819d7c46390eb97230f60b960b1e2b2c229
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-01-05 15:24:49 -0500

    Add unused suppress to KotlinModuleVersionAdapter

--- src/kotlin-1.5/kotlin/com/fasterxml/jackson/module/kotlin/KotlinModuleVersionAdapter.kt
+++ src/kotlin-1.5/kotlin/com/fasterxml/jackson/module/kotlin/KotlinModuleVersionAdapter.kt
@@ -2,6 +2,10 @@ package com.fasterxml.jackson.module.kotlin
 
 import com.fasterxml.jackson.databind.Module
 
+/**
+ * Called via reflection by [KotlinModule].
+ */
+@Suppress("unused")
 internal object KotlinModuleVersionAdapter {
     fun configureModuleSetupContext(context: Module.SetupContext) {
         context.addDeserializers(KotlinDeserializers_1_5())
