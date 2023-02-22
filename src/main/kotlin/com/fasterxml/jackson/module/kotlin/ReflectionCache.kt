d75498cc99f9a276994d314ac909d3e5bfb4a3ad
wrongwrong <boranti1995@gmail.com>
2023-02-23 01:40:11 +0900

    Add comment

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCache.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCache.kt
@@ -14,6 +14,7 @@ import kotlin.reflect.jvm.kotlinFunction
 
 internal class ReflectionCache(reflectionCacheSize: Int) : Serializable {
     companion object {
+        // Increment is required when properties that use LRUMap are changed.
         private const val serialVersionUID = 1L
     }
 
