50b29d999c358aedf2ce7eab58b55c589b9038da
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-03-14 18:23:30 -0700

    Minor test cleanup to ease merging into master/3.0

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/TestCommons.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/TestCommons.kt
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.module.kotlin
 
+import com.fasterxml.jackson.core.PrettyPrinter
 import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -7,7 +8,7 @@ import com.fasterxml.jackson.databind.ObjectWriter
 
 // This `printer` is used to match the output from Jackson to the newline char of the source code.
 // If this is removed, comparisons will fail in a Windows-like platform.
-val LF_PRINTER: DefaultPrettyPrinter =
+val LF_PRINTER: PrettyPrinter =
     DefaultPrettyPrinter().withObjectIndenter(DefaultIndenter().withLinefeed("\n"))
 
-fun ObjectMapper.testPrettyWriter(): ObjectWriter = this.writer(LF_PRINTER)
+fun ObjectMapper.testPrettyWriter(): ObjectWriter = this.writer().with(LF_PRINTER)
