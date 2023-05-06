64213dc98bc4b689ff0a29e8c214d556b8e9f3a4
wrongwrong <boranti1995@gmail.com>
2023-05-06 19:18:55 +0900

    Add test cases

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/SequenceSerdesTests.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/SequenceSerdesTests.kt
@@ -1,5 +1,9 @@
 package com.fasterxml.jackson.module.kotlin.test
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.Test
@@ -41,5 +45,40 @@ class TestSequenceDeserializer {
         val objectMapper =  jacksonObjectMapper()
         val result = objectMapper.writeValueAsString(data)
         assertEquals("{\"value\":[]}", result)
+    }
+
+    class ContentSer : StdSerializer<String>(String::class.java) {
+        override fun serialize(value: String, gen: JsonGenerator, provider: SerializerProvider) {
+            provider.defaultSerializeValue("$value-ser", gen)
+        }
+    }
+
+    data class ListWrapper(
+        @JsonSerialize(contentUsing = ContentSer::class) val value: List<String>
+    )
+
+    data class SequenceWrapper(
+        @JsonSerialize(contentUsing = ContentSer::class)
+        val value: Sequence<String>
+    )
+
+    @Test
+    fun contentUsingTest() {
+        val mapper = jacksonObjectMapper()
+
+        val listResult = mapper.writeValueAsString(ListWrapper(listOf("foo")))
+        val sequenceResult = mapper.writeValueAsString(SequenceWrapper(sequenceOf("foo")))
+
+        assertEquals("""{"value":["foo-ser"]}""", sequenceResult)
+        assertEquals(listResult, sequenceResult)
+    }
+
+    // @see #674
+    @Test
+    fun sequenceOfTest() {
+        val mapper = jacksonObjectMapper()
+        val result = mapper.writeValueAsString(sequenceOf("foo"))
+
+        assertEquals("""["foo"]""", result)
     }
 }
