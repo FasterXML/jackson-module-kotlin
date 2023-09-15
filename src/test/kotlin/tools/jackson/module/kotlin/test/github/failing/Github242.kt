de9d1b5a58770111c8f031ad2e009c90f5b1e1d8
wrongwrong <boranti1995@gmail.com>
2023-09-16 06:46:04 +0900

    Fix for merge error in #709

--- src/test/kotlin/tools/jackson/module/kotlin/test/github/failing/Github242.kt
+++ src/test/kotlin/tools/jackson/module/kotlin/test/github/failing/Github242.kt
@@ -1,8 +1,8 @@
 package tools.jackson.module.kotlin.test.github.failing
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.Test
+import tools.jackson.module.kotlin.jacksonObjectMapper
+import tools.jackson.module.kotlin.readValue
 import kotlin.test.assertEquals
 
 // Also see https://github.com/FasterXML/jackson-databind/issues/3392
@@ -14,9 +14,9 @@ class Github242 {
     // Since `nullish` is entered for a `non-null` value, deserialization is expected to fail,
     // but at the moment the process continues with the default value set on the `databind`.
     @Test
-    fun `test value throws - Int`(){
-        val v0 =objectMapper.readValue<IntValue>("{}")
-        val v1 =objectMapper.readValue<IntValue>("{\"value\":null}")
+    fun `test value throws - Int`() {
+        val v0 = objectMapper.readValue<IntValue>("{}")
+        val v1 = objectMapper.readValue<IntValue>("{\"value\":null}")
 
         assertEquals(0, v0.value)
         assertEquals(v0, v1)
