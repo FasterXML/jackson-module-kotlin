f8c7b4b9d03724d254cc5a9c0d447c58e0a2abac
wrongwrong <boranti1995@gmail.com>
2023-11-20 20:40:13 +0900

    Resolved differences from 2.x.

--- src/test/kotlin/tools/jackson/module/kotlin/test/github/failing/Github340.kt
+++ src/test/kotlin/tools/jackson/module/kotlin/test/github/failing/Github340.kt
@@ -1,9 +1,7 @@
 package tools.jackson.module.kotlin.test.github.failing
 
-import tools.jackson.databind.exc.UnrecognizedPropertyException
 import tools.jackson.module.kotlin.jacksonObjectMapper
 import tools.jackson.module.kotlin.readValue
-import tools.jackson.module.kotlin.test.expectFailure
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -20,15 +18,27 @@ class OwnerRequestTest {
 
     @Test
     fun testDeserHit340() {
-        expectFailure<UnrecognizedPropertyException>("GitHub #340 has been fixed!") {
-            val value: IsField = jackson.readValue(json)
-            assertEquals("Got a foo", value.foo)
-        }
+        val value: IsField = jackson.readValue(json)
+        // Fixed
+        assertEquals("Got a foo", value.foo)
     }
 
     @Test
     fun testDeserWithoutIssue() {
         val value: NoIsField = jackson.readValue(json)
         assertEquals("Got a foo", value.foo)
+    }
+
+    // A test case for isSetter to work, added with the fix for this issue.
+    class IsSetter {
+        lateinit var isFoo: String
+    }
+
+    @Test
+    fun isSetterTest() {
+        val json = """{"isFoo":"bar"}"""
+        val isSetter: IsSetter = jackson.readValue(json)
+
+        assertEquals("bar", isSetter.isFoo)
     }
 }
