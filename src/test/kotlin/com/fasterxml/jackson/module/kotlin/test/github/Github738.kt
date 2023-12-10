b7cc1a926a292bd96dc8bbc1e2c662c54e8b67e0
wrongwrong <boranti1995@gmail.com>
2023-12-10 12:51:33 +0900

    Make nulls = FAIL work even for primitives and missing
    Fixed bug by #738

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github738.kt
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.module.kotlin.test.github
+
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class Github738 {
+    data class D(@JsonSetter(nulls = Nulls.FAIL) val v: Int)
+
+    @Test
+    fun test() {
+        val mapper = jacksonObjectMapper()
+        // nulls = FAIL is reflected if it is primitive and missing
+        assertThrows(MismatchedInputException::class.java) { mapper.readValue<D>("{}") }
+    }
+}
