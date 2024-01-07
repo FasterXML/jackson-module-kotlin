db803238c228e2af8bdb901c55add3ac69d2d153
wrongwrong <boranti1995@gmail.com>
2024-01-07 16:27:39 +0900

    Add test cases
    from: https://github.com/ProjectMapK/jackson-module-kogera/tree/develop/src/test/kotlin/io/github/projectmapk/jackson/module/kogera/zIntegration/deser/parameterSize

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/TestCommons.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/TestCommons.kt
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectWriter
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.test.assertEquals
 
 // This `printer` is used to match the output from Jackson to the newline char of the source code.
 // If this is removed, comparisons will fail in a Windows-like platform.
@@ -12,3 +16,17 @@ val LF_PRINTER: PrettyPrinter =
     DefaultPrettyPrinter().withObjectIndenter(DefaultIndenter().withLinefeed("\n"))
 
 fun ObjectMapper.testPrettyWriter(): ObjectWriter = this.writer().with(LF_PRINTER)
+internal val defaultMapper = jacksonObjectMapper()
+
+internal inline fun <reified T : Any> callPrimaryConstructor(mapper: (KParameter) -> Any? = { it.name }): T =
+    T::class.primaryConstructor!!.run {
+        val args = parameters.associateWith { mapper(it) }
+        callBy(args)
+    }
+
+// Function for comparing non-data classes.
+internal inline fun <reified T : Any> assertReflectEquals(expected: T, actual: T) {
+    T::class.memberProperties.forEach {
+        assertEquals(it.get(expected), it.get(actual))
+    }
+}
