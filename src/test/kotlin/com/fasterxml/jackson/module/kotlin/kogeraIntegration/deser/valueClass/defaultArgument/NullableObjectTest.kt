881fbc225bf44954d551217a61d5c28c296e340c
wrongwrong <boranti1995@gmail.com>
2024-02-12 12:35:08 +0900

    Add value class default argument tests

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/deser/valueClass/defaultArgument/NullableObjectTest.kt
@@ -0,0 +1,56 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass.defaultArgument
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass.NullableObject
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import kotlin.test.Test
+
+class NullableObjectTest {
+    companion object {
+        val mapper = jacksonObjectMapper()
+    }
+
+    data class ByConstructor(
+        val nnNn: NullableObject = NullableObject("foo"),
+        val nnN: NullableObject = NullableObject(null),
+        val nNn: NullableObject? = NullableObject("bar"),
+        val nN: NullableObject? = null
+    )
+
+    @Test
+    fun byConstructorTestFailing() {
+        // #761(KT-57357) fixed
+        assertThrows(Error::class.java) {
+            assertEquals(ByConstructor(), mapper.readValue<ByConstructor>("{}"))
+        }
+    }
+
+    data class ByFactory(
+        val nnNn: NullableObject = NullableObject("foo"),
+        val nnN: NullableObject = NullableObject(null),
+        val nNn: NullableObject? = NullableObject("bar"),
+        val nN: NullableObject? = null
+    ) {
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun creator(
+                nn: NullableObject = NullableObject("foo"),
+                nnN: NullableObject = NullableObject(null),
+                nNn: NullableObject? = NullableObject("bar"),
+                nN: NullableObject? = null
+            ) = ByFactory(nn, nnN, nNn, nN)
+        }
+    }
+
+    @Test
+    fun byFactoryTest() {
+        // #761(KT-57357) fixed
+        assertThrows(Error::class.java) {
+            assertEquals(ByFactory.creator(), mapper.readValue<ByFactory>("{}"))
+        }
+    }
+}
