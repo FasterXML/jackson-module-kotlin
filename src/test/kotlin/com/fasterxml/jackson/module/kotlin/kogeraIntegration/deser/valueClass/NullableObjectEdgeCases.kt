6a961f7d5919aafbd2e6eb590fee18d3470ac985
wrongwrong <boranti1995@gmail.com>
2024-02-12 12:31:52 +0900

    Add value class top level tests

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/deser/valueClass/NullableObjectEdgeCases.kt
@@ -0,0 +1,69 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass
+
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.module.kotlin.WrapsNullableValueClassDeserializer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import kotlin.reflect.jvm.internal.KotlinReflectionInternalError
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class NullableObjectEdgeCases {
+    @JvmInline
+    value class VC(val v: String?)
+
+    class NullValueDeserializer : StdDeserializer<VC>(VC::class.java) {
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext): VC =
+            TODO("Not yet implemented")
+
+        override fun getNullValue(ctxt: DeserializationContext): VC = nv
+
+        companion object {
+            val nv = VC("nv")
+        }
+    }
+
+    data class NullValue(
+        @field:JsonDeserialize(using = NullValueDeserializer::class)
+        val nn: VC,
+        @field:JsonDeserialize(using = NullValueDeserializer::class)
+        val n: VC?
+    )
+
+    @Test
+    fun nullValueIsUsedPreferentially() {
+        val result = jacksonObjectMapper().readValue<NullValue>("""{"nn":null,"n":null}""")
+        assertEquals(NullValue(NullValueDeserializer.nv, NullValueDeserializer.nv), result)
+    }
+
+    class NullsSkipDeserializerWrapsNullable : WrapsNullableValueClassDeserializer<VC>(VC::class) {
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext): VC {
+            TODO("Not yet implemented")
+        }
+
+        override fun getBoxedNullValue(): VC? = null
+    }
+
+    data class NullsSkip(
+        @field:JsonSetter(nulls = Nulls.SKIP)
+        @field:JsonDeserialize(using = NullsSkipDeserializerWrapsNullable::class)
+        val nn: VC = VC("skip"),
+        @field:JsonSetter(nulls = Nulls.SKIP)
+        val n: VC? = VC("skip")
+    )
+
+    // There is a problem with #51, so it is a failing test.
+    @Test
+    fun `Nulls_SKIP works`() {
+        assertThrows("#761(KT-57357) fixed", KotlinReflectionInternalError::class.java) {
+            val result = jacksonObjectMapper().readValue<NullsSkip>("""{"nn":null,"n":null}""")
+            assertEquals(NullValue(VC("skip"), VC("skip")), result)
+        }
+    }
+}
---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/deser/valueClass/WithoutCustomDeserializeMethodTest.kt
@@ -0,0 +1,113 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass
+
+import com.fasterxml.jackson.module.kotlin.defaultMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Assert.assertTrue
+import org.junit.Ignore
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
+import java.lang.reflect.InvocationTargetException
+import kotlin.test.Test
+
+@RunWith(Enclosed::class)
+class WithoutCustomDeserializeMethodTest {
+    @Ignore
+    companion object {
+        val mapper = jacksonObjectMapper()
+        val throwable = IllegalArgumentException("test")
+    }
+
+    class DirectDeserializeTest {
+        @Test
+        fun primitive() {
+            val result = defaultMapper.readValue<Primitive>("1")
+            assertEquals(Primitive(1), result)
+        }
+
+        @Test
+        fun nonNullObject() {
+            val result = defaultMapper.readValue<NonNullObject>(""""foo"""")
+            assertEquals(NonNullObject("foo"), result)
+        }
+
+        @Suppress("ClassName")
+        class NullableObject_ {
+            @Test
+            fun value() {
+                val result = defaultMapper.readValue<NullableObject>(""""foo"""")
+                assertEquals(NullableObject("foo"), result)
+            }
+
+            // failing
+            @Test
+            fun nullString() {
+                // #209 has been fixed.
+                assertThrows(NullPointerException::class.java) {
+                    val result = defaultMapper.readValue<NullableObject>("null")
+                    assertEquals(NullableObject(null), result)
+                }
+            }
+        }
+    }
+
+    @Ignore
+    data class Dst(
+        val pNn: Primitive,
+        val pN: Primitive?,
+        val nnoNn: NonNullObject,
+        val nnoN: NonNullObject?,
+        val noNn: NullableObject,
+        val noN: NullableObject?
+    )
+
+    @Test
+    fun withoutNull() {
+        val expected = Dst(
+            Primitive(1),
+            Primitive(2),
+            NonNullObject("foo"),
+            NonNullObject("bar"),
+            NullableObject("baz"),
+            NullableObject("qux")
+        )
+        val src = mapper.writeValueAsString(expected)
+        val result = mapper.readValue<Dst>(src)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun withNull() {
+        val expected = Dst(
+            Primitive(1),
+            null,
+            NonNullObject("foo"),
+            null,
+            NullableObject(null),
+            null
+        )
+        val src = mapper.writeValueAsString(expected)
+        val result = mapper.readValue<Dst>(src)
+
+        assertEquals(expected, result)
+    }
+
+    @Ignore
+    @JvmInline
+    value class HasCheckConstructor(val value: Int) {
+        init {
+            if (value < 0) throw throwable
+        }
+    }
+
+    @Test
+    fun callConstructorCheckTest() {
+        val e = assertThrows(InvocationTargetException::class.java) { defaultMapper.readValue<HasCheckConstructor>("-1") }
+        assertTrue(e.cause === throwable)
+    }
+
+    // If all JsonCreator tests are OK, no need to check throws from factory functions.
+}
