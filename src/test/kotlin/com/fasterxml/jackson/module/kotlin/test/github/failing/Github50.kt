df93fde9d49f63b3ff9e91ca0a3a12055a30625c
joohyukkim <beanskobe@gmail.com>
2024-11-14 10:53:13 +0900

    Fixed tests

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/failing/Github50.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/failing/Github50.kt
@@ -19,10 +19,8 @@ class TestGithub50 {
     @Test
     fun testGithub50UnwrappedError() {
         val json = """{"firstName":"John","lastName":"Smith","position":"Manager"}"""
-        expectFailure<InvalidDefinitionException>("GitHub #50 has been fixed!") {
-            val obj: Employee = jacksonObjectMapper().readValue(json)
-            assertEquals(Name("John", "Smith"), obj.name)
-            assertEquals("Manager", obj.position)
-        }
+        val obj: Employee = jacksonObjectMapper().readValue(json)
+        assertEquals(Name("John", "Smith"), obj.name)
+        assertEquals("Manager", obj.position)
     }
 }
