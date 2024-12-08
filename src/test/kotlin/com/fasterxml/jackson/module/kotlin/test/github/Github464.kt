4d267fce05439f43a9ed5c1ed9889aa6b3ed5ef1
wrongwrong <boranti1995@gmail.com>
2024-12-08 20:55:56 +0900

    Migrate to JUnit5 notation and remove use of @Ignore

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github464.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/Github464.kt
@@ -12,47 +12,56 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.testPrettyWriter
-import org.junit.Ignore
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Github464 {
-    class UnboxTest {
-        object NullValueClassKeySerializer : StdSerializer<ValueClass>(ValueClass::class.java) {
-            override fun serialize(value: ValueClass?, gen: JsonGenerator, provider: SerializerProvider) {
-                gen.writeFieldName("null-key")
-            }
+    object NullValueClassKeySerializer : StdSerializer<ValueClass>(ValueClass::class.java) {
+        override fun serialize(value: ValueClass?, gen: JsonGenerator, provider: SerializerProvider) {
+            gen.writeFieldName("null-key")
         }
+    }
 
-        interface IValue
-        @JvmInline
-        value class ValueClass(val value: Int?) : IValue
-        data class WrapperClass(val inlineField: ValueClass)
+    interface IValue
 
-        abstract class AbstractGetter<T> {
-            abstract val qux: T
+    @JvmInline
+    value class ValueClass(val value: Int?) : IValue
+    data class WrapperClass(val inlineField: ValueClass)
 
-            fun <T> getPlugh() = qux
+    abstract class AbstractGetter<T> {
+        abstract val qux: T
+
+        fun <T> getPlugh() = qux
+    }
+    interface IGetter<T> {
+        val quux: T
+
+        fun <T> getXyzzy() = quux
+    }
+
+    class Poko(
+        val foo: ValueClass,
+        val bar: ValueClass?,
+        val baz: IValue,
+        override val qux: ValueClass,
+        override val quux: ValueClass,
+        val corge: Collection<ValueClass?>,
+        val grault: Array<ValueClass?>,
+        val garply: WrapperClass,
+        val waldo: WrapperClass?,
+        val fred: Map<ValueClass, ValueClass?>
+    ) : AbstractGetter<ValueClass>(), IGetter<ValueClass>
+
+    object NullValueSerializer : StdSerializer<Any>(Any::class.java) {
+        override fun serialize(value: Any?, gen: JsonGenerator, provider: SerializerProvider) {
+            gen.writeString("null-value")
         }
-        interface IGetter<T> {
-            val quux: T
+    }
 
-            fun <T> getXyzzy() = quux
-        }
-
-
-        class Poko(
-            val foo: ValueClass,
-            val bar: ValueClass?,
-            val baz: IValue,
-            override val qux: ValueClass,
-            override val quux: ValueClass,
-            val corge: Collection<ValueClass?>,
-            val grault: Array<ValueClass?>,
-            val garply: WrapperClass,
-            val waldo: WrapperClass?,
-            val fred: Map<ValueClass, ValueClass?>
-        ) : AbstractGetter<ValueClass>(), IGetter<ValueClass>
+    @Nested
+    inner class UnboxTest {
 
         private val zeroValue = ValueClass(0)
         private val oneValue = ValueClass(1)
@@ -105,12 +114,6 @@ class Github464 {
             )
         }
 
-        object NullValueSerializer : StdSerializer<Any>(Any::class.java) {
-            override fun serialize(value: Any?, gen: JsonGenerator, provider: SerializerProvider) {
-                gen.writeString("null-value")
-            }
-        }
-
         @Test
         fun nullValueSerializerTest() {
             @Suppress("UNCHECKED_CAST")
@@ -148,20 +151,22 @@ class Github464 {
         }
     }
 
-    class SerializerPriorityTest {
-        @JvmInline
-        value class ValueBySerializer(val value: Int)
+    @JvmInline
+    value class ValueBySerializer(val value: Int)
 
-        object Serializer : StdSerializer<ValueBySerializer>(ValueBySerializer::class.java) {
-            override fun serialize(value: ValueBySerializer, gen: JsonGenerator, provider: SerializerProvider) {
-                gen.writeString(value.value.toString())
-            }
+    object Serializer : StdSerializer<ValueBySerializer>(ValueBySerializer::class.java) {
+        override fun serialize(value: ValueBySerializer, gen: JsonGenerator, provider: SerializerProvider) {
+            gen.writeString(value.value.toString())
         }
-        object KeySerializer : StdSerializer<ValueBySerializer>(ValueBySerializer::class.java) {
-            override fun serialize(value: ValueBySerializer, gen: JsonGenerator, provider: SerializerProvider) {
-                gen.writeFieldName(value.value.toString())
-            }
+    }
+    object KeySerializer : StdSerializer<ValueBySerializer>(ValueBySerializer::class.java) {
+        override fun serialize(value: ValueBySerializer, gen: JsonGenerator, provider: SerializerProvider) {
+            gen.writeFieldName(value.value.toString())
         }
+    }
+
+    @Nested
+    inner class SerializerPriorityTest {
 
         private val target = mapOf(ValueBySerializer(1) to ValueBySerializer(2))
         private val sm = SimpleModule()
@@ -178,7 +183,6 @@ class Github464 {
         // Currently, there is a situation where the serialization results are different depending on the registration order of the modules.
         // This problem is not addressed because the serializer registered by the user has priority over Extensions.kt,
         // since KotlinModule is basically registered first.
-        @Ignore
         @Test
         fun priorityTest() {
             val km = KotlinModule.Builder().build()
@@ -187,7 +191,7 @@ class Github464 {
 
             // om1(collect) -> """{"1":"2"}"""
             // om2(broken)  -> """{"1":2}"""
-            assertEquals(om1.writeValueAsString(target), om2.writeValueAsString(target))
+            assertNotEquals(om1.writeValueAsString(target), om2.writeValueAsString(target))
         }
     }
 }
