f391a8339b56c0388bf7afe35f9bec178165089d
wrongwrong <boranti1995@gmail.com>
2025-02-02 14:02:56 +0900

    Simplify
    defaultSerializeValue has null value consideration.
    https://github.com/FasterXML/jackson-databind/blob/a204131005008e42d953a929499089f7f5221218/src/main/java/com/fasterxml/jackson/databind/SerializerProvider.java#L1177-L1182

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinSerializers.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinSerializers.kt
@@ -56,12 +56,6 @@ object ValueClassUnboxSerializer : StdSerializer<Any>(Any::class.java) {
 
     override fun serialize(value: Any, gen: JsonGenerator, provider: SerializerProvider) {
         val unboxed = value::class.java.getMethod("unbox-impl").invoke(value)
-
-        if (unboxed == null) {
-            provider.defaultSerializeNull(gen)
-            return
-        }
-
         provider.defaultSerializeValue(unboxed, gen)
     }
 }
@@ -76,9 +70,7 @@ internal sealed class ValueClassSerializer<T : Any>(t: Class<T>) : StdSerializer
             val unboxed = unboxMethod.invoke(value)
             // As shown in the processing of the factory function, jsonValueGetter is always a static method.
             val jsonValue: Any? = staticJsonValueGetter.invoke(null, unboxed)
-            jsonValue
-                ?.let { provider.defaultSerializeValue(it, gen) }
-                ?: provider.defaultSerializeNull(gen)
+            provider.defaultSerializeValue(jsonValue, gen)
         }
     }
 
